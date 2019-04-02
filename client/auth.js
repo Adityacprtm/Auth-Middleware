14c2cb0c3d0ea769b5b7cbc3ab4bd41313e4716e
Aditya Chamim Pratama <pratamaditya7@gmail.com>
2019-04-03 00:01:38 +0700

    update

--- client/auth.js
+++ client/auth.js
@@ -1,9 +1,13 @@
+var uuid = require('uuid')
+var randomMac = require('random-mac')
+
 var request = require('request'),
-    username = "1234",
+    username = "user",
     password = "secret_key",
+    //username = "6aba8c90a10f23edf3f82c7a0c77e9bc9125eb66",
+    //password = "c11db09efa0603a4803e903682af40ae",
     url = "http://127.0.0.1:8080/",
-    auth = "Basic " + new Buffer.from((username + ":" + password).toString())
-    //auth = "Basic " + username + ":" + password;
+    auth = "Basic " + new Buffer.from(username + ":" + password).toString('base64')
 
 var optionsDevice = {
     url: url,
@@ -14,35 +18,38 @@ var optionsDevice = {
     },
     json: true,
     body: {
-        "id": "1234",
+        "id": "5678",
         "device": "nodemcu",
-        "mac": "789"
+        "mac": "0987"
     }
 };
 
-// var optionsUser = {
-//     uri: 'http://127.0.0.1:8080/admin/login',
-//     method: 'PUT',
-//     json: {
-//         "username": "user",
-//         "password": "secret",
-//         "device": {
-//             "id": "5678",
-//             "secret": "secret2",
-//             "device": "arduino",
-//             "mac": "321"
-//         }
-//     }
-// }
+var optionsUser = {
+    uri: url + "admin/login",
+    method: 'POST',
+    headers: {
+        'content-type': 'application/json',
+        'Authorization': auth
+    },
+    json: true,
+    body: {
+        "username": "admin",
+        "device": {
+            "id": uuid(),
+            "device": "nodemcu",
+            "mac": randomMac()
+        }
+    }
+}
 
-request(optionsDevice, function (error, response, body) {
-    if (error) console.log('ERROR: ' + error);
-    console.log('statusCode:', response && response.statusCode);
-    console.log('body:', body);
-});
-
-// request(optionsUser, function (error, response, body) {
+// request(optionsDevice, function (error, response, body) {
 //     if (error) console.log('ERROR: ' + error);
 //     console.log('statusCode:', response && response.statusCode);
 //     console.log('body:', body);
-// })
+// });
+
+request(optionsUser, function (error, response, body) {
+    if (error) console.log('ERROR: ' + error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+})
---
+++ client/mqtt_client.js
@@ -0,0 +1,117 @@
+var mqtt, optionsMqtt, optionsAuth, client, request
+var username, password, url, auth, uuid, randomMac,
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImEwN2FjOWNiLTQ0MjgtNDY5MC05MzQzLWMyYTQzM2U2YzAwOSIsImRldmljZSI6Im5vZGVtY3UiLCJtYWMiOiI1NDo1MjowMDo4Yjo5OTplMCIsImlwIjoiOjpmZmZmOjEyNy4wLjAuMSIsInRpbXN0YW1wIjoxNTU0MTc4MjAyOTg5LCJpYXQiOjE1NTQxNzgyMDIsImV4cCI6MTU1NDE4MTgwMn0.SXLqjPMWUbYhkwWjB78Aqc2McvCG1eIPbcLX3vrIs9Y"
+
+mqtt = require('mqtt')
+request = require('request')
+uuid = require('uuid')
+randomMac = require('random-mac')
+username = "176b4593c90d3f94876f42f1324e9dafaba31926"
+password = "b56d730cace47884eb11ffa7603fe777"
+url = "http://127.0.0.1:8080/"
+auth = "Basic " + new Buffer.from((username + ":" + password).toString())
+
+optionsAuth = {
+    url: url,
+    method: 'POST',
+    headers: {
+        'content-type': 'application/json',
+        'Authorization': auth
+    },
+    json: true,
+    body: {
+        "id": "a07ac9cb-4428-4690-9343-c2a433e6c009",
+        "device": "nodemcu",
+        "mac": "54:52:00:8b:99:e0"
+    }
+}
+
+if (token == null) {
+    request(optionsAuth, function (error, response, body) {
+        if (error) console.log('ERROR: ' + error)
+        if (response.statusCode == 200) {
+            // token = body.token
+            optionsMqtt = {
+                port: 1883,
+                username: token,
+                pasword: '',
+                keepalive: 10000
+            }
+            client = mqtt.connect('mqtt://127.0.0.1', optionsMqtt)
+            client.on('connect', () => {
+                console.log('Connected')
+                setInterval(function () {
+                    var topic = 'office'
+                    var payload = {
+                        protocol: client.options.protocol,
+                        timestamp: new Date().getTime().toString(),
+                        topic: topic,
+                        sensor: {
+                            tipe: "4325",
+                            index: "string",
+                            ip: client.options.host,
+                            module: "string"
+                        },
+                        humidity: {
+                            value: Math.floor(Math.random() * 100),
+                            unit: "string"
+                        },
+                        temperature: {
+                            value: Math.floor(Math.random() * 100),
+                            unit: "string"
+                        }
+                    }
+                    client.publish(topic, JSON.stringify(payload), [1]);
+                    console.log('Message Sent ' + topic);
+                }, 5000);
+            })
+        } else {
+            console.log('statusCode:', response && response.statusCode)
+            console.log('body:', body)
+        }
+    });
+} else {
+    optionsMqtt = {
+        port: 1883,
+        username: token,
+        pasword: '',
+        keepalive: 10000
+    }
+    client = mqtt.connect('mqtt://127.0.0.1', optionsMqtt)
+    client.on('connect', () => {
+        console.log('Connected')
+        setInterval(function () {
+            var topic = 'office'
+            var payload = {
+                protocol: client.options.protocol,
+                timestamp: new Date().getTime().toString(),
+                topic: topic,
+                sensor: {
+                    tipe: "4325",
+                    index: "string",
+                    ip: client.options.host,
+                    module: "string"
+                },
+                humidity: {
+                    value: Math.floor(Math.random() * 100),
+                    unit: "string"
+                },
+                temperature: {
+                    value: Math.floor(Math.random() * 100),
+                    unit: "string"
+                }
+            }
+            client.publish(topic, JSON.stringify(payload), [1]);
+            console.log('Message Sent ' + topic);
+        }, 5000);
+    })
+}
+
+// client.on('close', (error) => {
+//     if (error) console.log(error)
+// });
+
+// client.on('error', (error) => {
+//     if (error) console.log(error.toString())
+//     client.end(true)
+// })
